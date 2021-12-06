f50f7a79ea81462d9e96282772c7efcf7153800b
slava64 <slava0sva@gmail.com>
2021-12-06 11:04:05 +0400

    iterator

--- pizza/PizzaOrder.php
+++ pizza/PizzaOrder.php
@@ -59,28 +59,4 @@ class PizzaOrder
     {
         return $this->getPrice() - $this->getDiscount();
     }
-
-    /*public function getPrice(\pizza\discount\BaseDiscount $baseDiscount)
-    {
-        if ($baseDiscount instanceof \pizza\discount\AmountDiscount) {
-            $pizzaList = $this->getPizzaList();
-            if(count($pizzaList) > 5) {
-                $baseDiscount->setCountFree(2);
-            } elseif (count($pizzaList) > 3) {
-                $baseDiscount->setCountFree(1);
-            }
-            return $baseDiscount->calc();
-        } elseif ($baseDiscount instanceof \pizza\discount\SumDiscount) {
-            $discount = new \pizza\discount\BaseDiscount($this);
-            $basePrice = $discount->calc();
-            if($basePrice > 2000) {
-                $baseDiscount->setPercent(20);
-            } elseif ($basePrice > 1000) {
-                $baseDiscount->setPercent(10);
-            }
-            return $baseDiscount->calc();
-        } else {
-            return $baseDiscount->calc();
-        }
-    }*/
 }
